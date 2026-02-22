9f66fc322bec3ff51d47d5918b4eed8571b287e3
Shruti H. <167879533+shruti-harayan@users.noreply.github.com>
2026-02-22 18:19:02 +0530

    Add files via upload

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,10 +3,10 @@ import React from "react";
 import { type User, type TokenResponse } from "../services/authService";
 import { setAuthToken } from "../services/api";
 
-
 type AuthContextType = {
   user: User | null;
   token: string | null;
+  refreshToken: string | null;
   loading: boolean;
   loginFromResponse: (data: TokenResponse) => void;
   logout: () => void;
@@ -14,20 +14,28 @@ type AuthContextType = {
 
 const AuthContext = React.createContext<AuthContextType | undefined>(undefined);
 
-export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [user, setUser] = React.useState<User | null>(null);
   const [token, setToken] = React.useState<string | null>(null);
   const [loading, setLoading] = React.useState(true);
+  const [refreshToken, setRefreshToken] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     // Restore from localStorage on page refresh
     const storedToken = localStorage.getItem("gf_token");
     const storedUser = localStorage.getItem("gf_user");
+    const storedRefresh = localStorage.getItem("gf_refresh");
 
+    if (storedRefresh) {
+      setRefreshToken(storedRefresh);
+    }
     if (storedToken) {
       setToken(storedToken);
-      // helper to set axios header (your api helper)
       setAuthToken(storedToken);
+    } else {
+      setAuthToken(null);
     }
     if (storedUser) {
       try {
@@ -41,32 +49,42 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
   function loginFromResponse(data: TokenResponse) {
     const t = data.access_token;
+    const r = data.refresh_token;
     const u = data.user;
-    // persist
+
     localStorage.setItem("gf_token", t);
+    localStorage.setItem("gf_refresh", r);
     localStorage.setItem("gf_user", JSON.stringify(u));
-    // set axios default header for future requests
+
     setAuthToken(t);
-    // update React state
+
     setToken(t);
+    setRefreshToken(r);
     setUser(u);
   }
 
   function logout() {
     setToken(null);
+    setRefreshToken(null);
     setUser(null);
     setAuthToken(null);
+
     localStorage.removeItem("gf_token");
+    localStorage.removeItem("gf_refresh");
     localStorage.removeItem("gf_user");
   }
 
   return (
-    <AuthContext.Provider value={{ user, token, loading, loginFromResponse, logout }}>
+    <AuthContext.Provider
+      value={{ user, token, refreshToken, loading, loginFromResponse, logout }}
+    >
       {children}
     </AuthContext.Provider>
   );
 };
-
+export function getRefreshToken() {
+  return localStorage.getItem("gf_refresh");
+}
 export function useAuth() {
   const ctx = React.useContext(AuthContext);
   if (!ctx) {
